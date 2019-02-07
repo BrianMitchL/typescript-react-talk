2b1f42cbce06433487dc4096e5ce5805afbe0a8f
Brian Mitchell <bman4789@gmail.com>
2019-02-06 22:44:08 -0600

    React hooks! 🥳🎉

--- src/areas/pure/SimpleHelpers.tsx
+++ src/areas/pure/SimpleHelpers.tsx
@@ -1,65 +1,54 @@
-import React, { ChangeEventHandler, Component } from 'react';
+import React, { ChangeEventHandler, useState } from 'react';
 import { display } from './simple-helpers';
 
-interface SimpleHelpersState {
-  text: string;
-  num: number;
-}
+export const SimpleHelpers = () => {
+  const [text, setText] = useState('Hello JavaScript');
+  const [num, setNum] = useState(1);
 
-export class SimpleHelpers extends Component<{}, SimpleHelpersState> {
-  state = {
-    text: 'Hello JavaScriptMN',
-    num: 1
+  const handleNumberChange: ChangeEventHandler<HTMLInputElement> = e => {
+    setNum(Number(e.currentTarget.value));
   };
 
-  handleNumberChange: ChangeEventHandler<HTMLInputElement> = e => {
-    this.setState({ num: Number(e.currentTarget.value) });
+  const handleTextChange: ChangeEventHandler<HTMLInputElement> = e => {
+    setText(e.currentTarget.value);
   };
 
-  handleTextChange: ChangeEventHandler<HTMLInputElement> = e => {
-    this.setState({ text: e.currentTarget.value });
-  };
+  return (
+    <>
+      <h1>Simple Helpers</h1>
+      <fieldset className="uk-fieldset">
+        <legend className="uk-legend">Let's make some enthusiastic text</legend>
 
-  render() {
-    return (
-      <>
-        <h1>Simple Helpers</h1>
-        <fieldset className="uk-fieldset">
-          <legend className="uk-legend">
-            Let's make some enthusiastic text
-          </legend>
+        <div>
+          <label htmlFor="text">Text</label>
+          <input
+            type="text"
+            id="text"
+            className="uk-input"
+            autoComplete={'off'}
+            value={text}
+            onChange={handleTextChange}
+          />
+        </div>
 
-          <div>
-            <label htmlFor="text">Text</label>
-            <input
-              type="text"
-              id="text"
-              className="uk-input"
-              autoComplete={'off'}
-              value={this.state.text}
-              onChange={this.handleTextChange}
-            />
-          </div>
+        <div>
+          <label htmlFor="number">Enthusiasms</label>
+          <input
+            type="number"
+            id="number"
+            className="uk-input"
+            min={0}
+            step={1}
+            autoComplete={'off'}
+            value={num}
+            onChange={handleNumberChange}
+          />
+        </div>
+      </fieldset>
 
-          <div>
-            <label htmlFor="number">Enthusiasms</label>
-            <input
-              type="number"
-              id="number"
-              className="uk-input"
-              min={0}
-              step={1}
-              autoComplete={'off'}
-              value={this.state.num}
-              onChange={this.handleNumberChange}
-            />
-          </div>
-        </fieldset>
-
-        <p>
-          <strong>{display(this.state.text, this.state.num)}</strong>
-        </p>
-      </>
-    );
-  }
-}
+      <p>
+        <strong>{display(text, num)}</strong>
+      </p>
+    </>
+  );
+};
