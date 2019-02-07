c7cb5b2c59af681218bf62f5e180319be13b4f09
Brian Mitchell <bman4789@gmail.com>
2019-02-06 23:09:35 -0600

    Use functional component

--- src/areas/home/Home.tsx
+++ src/areas/home/Home.tsx
@@ -1,34 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logoR from './logo.svg';
 import logoTs from './ts-logo.svg';
 import './Home.css';
 
-export class Home extends Component {
-  render() {
-    return (
-      <div className="Home">
-        <img src={logoR} className="react-logo" alt="React logo" />
-        <img src={logoTs} className="typescript-logo" alt="TypeScript logo" />
-        <h1>
-          A (<small>tiny</small>) Intro to TypeScript with React
-        </h1>
-        <h2 className="uk-margin-remove">Brian Mitchell</h2>
-        <p>
-          Bird site:{' '}
-          <a
-            href="https://twitter.com/BrianMitchL"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {'@BrianMitchL'}
-          </a>
-          <br />
-          Web site:{' '}
-          <a href="https://brianm.me" target="_blank" rel="noopener noreferrer">
-            {'https://brianm.me'}
-          </a>
-        </p>
-      </div>
-    );
-  }
-}
+export const Home = () => (
+  <div className="Home">
+    <img src={logoR} className="react-logo" alt="React logo" />
+    <img src={logoTs} className="typescript-logo" alt="TypeScript logo" />
+    <h1>
+      A (<small>tiny</small>) Intro to TypeScript with React
+    </h1>
+    <h2 className="uk-margin-remove">Brian Mitchell</h2>
+    <p>
+      Bird site:{' '}
+      <a
+        href="https://twitter.com/BrianMitchL"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {'@BrianMitchL'}
+      </a>
+      <br />
+      Web site:{' '}
+      <a href="https://brianm.me" target="_blank" rel="noopener noreferrer">
+        {'https://brianm.me'}
+      </a>
+    </p>
+  </div>
+);
